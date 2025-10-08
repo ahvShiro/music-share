47f3564ca185768657588c85badfec463fd11440
ahvShiro <arthurshiro123@gmail.com>
2025-10-08 20:20:27 -0300

    feat: implemented password hashing function

---
+++ src/main/java/br/com/shiroshima/security/PasswordHasher.java
@@ -0,0 +1,16 @@
+package br.com.shiroshima.security;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHasher {
+
+    public static String hashPassword(String password) {
+        int logRounds = 10;
+        String salt = BCrypt.gensalt(logRounds);
+        return BCrypt.hashpw(password, salt);
+    }
+
+    public static boolean passwordMatchesHash(String password, String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
+}
