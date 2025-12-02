a9732ef9699b034ecaf580c322ea83ceb133bec3
ahvShiro <arthurshiro123@gmail.com>
2025-12-02 20:00:19 -0300

    chore: removed unused exceptions

--- src/main/java/br/com/shiroshima/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.shiroshima.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/shiroshima/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package br.com.shiroshima.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
