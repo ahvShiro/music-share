f2216a81642eb4c3872842855c11aab2b6ad9617
ahvShiro <arthurshiro123@gmail.com>
2025-12-02 00:35:13 -0300

    fix: main menu logic for logged-in users

--- src/main/java/br/com/shiroshima/Main.java
+++ src/main/java/br/com/shiroshima/Main.java
@@ -1,10 +1,14 @@
 package br.com.shiroshima;
 
 
+import br.com.shiroshima.controller.PostController;
 import br.com.shiroshima.controller.UserController;
+import br.com.shiroshima.repository.PostDAO;
 import br.com.shiroshima.repository.UserDAO;
+import br.com.shiroshima.service.PostService;
 import br.com.shiroshima.service.UserService;
 import br.com.shiroshima.view.MainView;
+import br.com.shiroshima.view.PostView;
 import br.com.shiroshima.view.UserView;
 
 public class Main {
@@ -14,9 +18,11 @@ public class Main {
         UserController controller = new UserController(service);
         UserView userView = new UserView(controller);
 
-        MainView view = new MainView(userView);
+        PostService postService = new PostService(new PostDAO());
+        PostController postController = new PostController(postService);
+        PostView postView = new PostView(postController);
 
-
+        MainView view = new MainView(userView, postView);
 
         view.runMenu();
     }
